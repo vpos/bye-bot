045ba5c7d913dbce33fe1840fecf2a68d391150e
vpos <vpospisil@fevo.com>
2020-06-20 14:04:30 +0200

    post route

--- api.py
+++ api.py
@@ -1,12 +1,24 @@
 import flask
 import bye as Cheems
 
+from flask import request
+
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
+@app.before_request
+def log_request_info():
+    print('Headers: %s', request.headers)
+    print('Body: %s', request.get_data())
 
 @app.route('/', methods=['GET'])
 def home():
+    return 'Pong'
+
+@app.route('/quit', methods=['POST'])
+def home():
+    log_request_info
     return Cheems.quits()
 
 app.run()
